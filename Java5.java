ec04b45c17d8869518d2b75f02461d1b5e7ce819
ChetanAhire11 <ahirechetan291@gmail.com>
2024-02-01 23:07:32 +0530

    Remove character

---
+++ Java5.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class Java5 {
+    static String removeChars(String string1, String string2){
+        // code here
+        HashSet <Character>hs=new HashSet<>();
+        for(int i=0;i<string2.length();i++){
+            hs.add(string2.charAt(i));
+        }
+        String ans="";
+        for(int i=0;i<string1.length();i++){
+            if(! hs.contains(string1.charAt(i))){
+                ans=ans+string1.charAt(i);
+                
+            }
+        }
+        return ans;
+    }
+}
